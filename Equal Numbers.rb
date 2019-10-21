83a36b231792fc452e407d144f448d7f89979a7a
SRGTxTwinkie <zanedreisbig@yahoo.com>
2019-10-21 17:20:04 -0400

    Complete

---
+++ Equal Numbers.rb
@@ -0,0 +1,19 @@
+def Equal
+
+  print "Number 1: "
+  num1 = gets.to_i
+
+  print "Number 2: "
+  num2 = gets.to_i
+
+  if num1 == num2
+    print "Ye \n"
+  else
+    print "Naw \n"
+  end
+
+end
+
+loop do
+  Equal()
+end
